cee93b03b59ab3349cfb1a86c5d2d06e7ca19348
张锦华 <colin.zhang@zsgcm.com>
2021-06-17 23:22:31 +0800

    add args

--- eason-goods/src/main/java/com/eason/goods/GoodsApplication.java
+++ eason-goods/src/main/java/com/eason/goods/GoodsApplication.java
@@ -10,6 +10,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication(scanBasePackages = {"com.cartisan", "com.eason.goods"})
 @EnableEurekaClient
 public class GoodsApplication {
-    public static void main(String[] args) {SpringApplication.run(GoodsApplication.class);
+    public static void main(String[] args) {SpringApplication.run(GoodsApplication.class, args);
     }
 }
